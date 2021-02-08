34debaeef20b5e87f88681a129d2f45ffa85355e
taoufiqq <t.elhanchaoui@gmail.com>
2021-02-08 17:01:36 +0100

    done

---
+++ backend/app.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+var cors = require('cors')
+var bodyParser = require('body-parser')
+
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/fastFoodDB' , {
+  useNewUrlParser: true
+}).then(() => {
+  console.log("Successfully connected to the database");    
+}).catch(err => {
+  console.log('Could not connect to the database. Exiting now...', err);
+  process.exit();
+});
+
+
+
+
+// import router 
+
+const categoryRoute = require('./routes/category');
+const sousCategoryRoute = require('./routes/sousCategory');
+const productRoute = require('./routes/product');
+
+app.use('/category' ,categoryRoute);
+app.use('/sousCategory' ,sousCategoryRoute);
+app.use('/product' ,productRoute);
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
